66f7178801ee9df3135faec5d3122a240d581811
gastipatis <gastipatis@gmail.com>
2023-07-18 16:58:38 +0300

    Do some fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // function envVarNamesToDefinitions(envVars) {
@@ -63,5 +63,5 @@ import react from '@vitejs/plugin-react-swc'
 export default () => {
   return defineConfig({
      plugins: [react()],
-  });
+  })
 }
