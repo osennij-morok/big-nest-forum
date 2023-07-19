a62eefc909e8b681e4d22ca3b15957b5a69dc0f1
gastipatis <gastipatis@gmail.com>
2023-07-19 14:02:13 +0300

    Fix database initialization

---
+++ db/init-db.sql
@@ -0,0 +1,2 @@
+--CREATE DATABASE big_nest_forum;
+CREATE TYPE account_role AS ENUM ('USER', 'MODER', 'ADMIN');
--- init-backend.sh
+++ init-backend.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cp .env ./backend/.env
 cd ./backend
 ./gradlew clean build
 cd -
